a2e8807b917a57510754499fc851769b102b60ae
Virgile Retault <virgile.retault@telecom-paris.fr>
2020-06-23 11:38:12 +0200

    removed pong.s and logo.s

--- src/init.s
+++ src/init.s
@@ -1,23 +1,3 @@
-.ROMDMG ;This is a Gameboy ROM
-.NAME "PONGDEMO" ;ROM name (11 char max)
-.CARTRIDGETYPE 0 ;Normal cartridge with 32ko of ROM
-.RAMSIZE 0 ;No save RAM (0ko)
-.COMPUTEGBCHECKSUM ;Won't boot without it
-.COMPUTEGBCOMPLEMENTCHECK ;Won't boot without it
-.LICENSEECODENEW "00" ;Dev license number (we don't have any)
-.EMPTYFILL $00 ;Fill unused ROM space with 0
-
-.MEMORYMAP ;We have two slots of 16ko, total 32ko
-SLOTSIZE $4000
-DEFAULTSLOT 0
-SLOT 0 $0000
-SLOT 1 $4000
-.ENDME
-
-.ROMBANKSIZE $4000
-.ROMBANKS 2
-.BANK 0 SLOT 0 ;Default slot at slot 0 (first 16 ko of ROM)
-
 /*$0040 is the interupt address*/
 .ORG $0040
 call VBlank ;Call directive Vblank
