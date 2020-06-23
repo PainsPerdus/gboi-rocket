ffb64c1db375801d20c4482be7f361ca617f63c5
melissa <melissa.blancheb@gmail.com>
2020-06-23 22:58:50 +0200

    get scale and time of the note to play

--- src/music.s
+++ src/music.s
@@ -10,27 +10,50 @@ dw timing1
 dw timing2
 
 timer_interupt :
-ld a, (rest1)
-dec a ;on soustrait la frequence du timer
-jp nz, pass1
 
-;quand la note est terminee
-ld hl, curs1
-ld b, (hl)
-inc hl
-ld c, (hl)
-inc bc ;(*curs1)++
-ld (hl), c
-dec hl
-ld (hl), b
+    ;check if the note is finished
+    ld a, (rest1)
+    dec a
+    jp nz, pass1
 
-ld a, (curs1)
+    ;if the note is finished, let's get the next one
+    ld hl, curs1
+    ld b, (hl); take higher weight bits
+    inc hl
+    ld c, (hl) ;take lower weight bits
+    inc bc ;take the address of the next note
+    ld (hl), b
 
-and $F0 ;recuperation des bits de poid fort
-swap
-ld scale1, a
+    ld a,(bc) ;take and the note to play
+    ld e, a
 
-and $0F ;recuperation des bits de poid faible
-ld timing1
+    ;take the scale of the note to play
+    and $F0 ;take higher weight bits which takes the scale of the note
+    swap
+    ld hl, scale1
+    ld b, (hl) ;take higher weight bits
+    inc hl
+    ld c, (hl) ;take lower weight bits
+    ld d, a
+    sla d
+    add bc, d ;find the the scale linked to a
+    ld a, (bc)
+    ld d, a ;save the frequency of the note to play in d
+
+    ld a, e :take the note to play
+
+    ;take the time of the note to play
+    and $0F ;take lower weight bits which takes the time of the note
+    ld hl, timing1
+    ld b, (hl) ;take higher weight bits
+    inc hl
+    ld c, (hl) ;take lower weight bits
+    ld e, a
+    sla e
+    add bc, e
+    ld a, (bc)
+    ld e, a ;save the time of the note to play in e
+
+    ;play the note
 
 pass1:
