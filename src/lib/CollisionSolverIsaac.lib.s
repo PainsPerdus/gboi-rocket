08b6e93a3b10cdeb9fd81d0b7cce55221d32d98d
Histauce <histauce@laposet.net>
2020-06-26 18:13:01 +0200

    Call collision solver

--- src/lib/CollisionSolverIsaac.lib.s
+++ src/lib/CollisionSolverIsaac.lib.s
@@ -4,9 +4,9 @@
 collisionSolverIsaac:
 	push bc
 	push de
-	
+
 	ld c, (hl) ;get element's size and flags
-	
+
 	;/// Blocking Collision \\\
 	bit 7, c
 	jp z, @noBlockCollision
@@ -14,6 +14,13 @@ collisionSolverIsaac:
 	;// Touch Horizontally \\
 	bit 7, b
 	jr z, @noHorizontalCollision
+
+	
+	ld a, 100
+	ld (global_.isaac.x), a
+	ld (global_.isaac.y), a
+
+
 	ld a, (global_.isaac.speed)
 	and %11110000
 	swap a
@@ -53,33 +60,33 @@ collisionSolverIsaac:
 @noVerticalCollision:
 	;\\\ Blocking Collision ///
 @noBlockCollision:
-	
+
 	; ld d, h
 	; ld e, l ;saving sheet address
-	
+
 ; 	;/// Hurting Collision \\\
 ; 	bit 6, c
 ; 	jr z, @noHurtCollision
 ; 	ld a, (global_.isaac.recover)
 ; 	cp 0
 ; 	jr z, @noHurtCollision
-	
+
 ; 	;hurting Isaac
 ; 	inc hl
 ; 	ld a, (global_.isaac.hp)
 ; 	sub (hl)
 ; 	ld (global_.isaac.hp), a
-	
+
 ; 	;starting recovery
 ; 	ld a, RECOVERYTIME
 ; 	ld (global_.isaac.recover), a
 ; 	;\\\ Hurting Collision ///
 ; @noHurtCollision:
-	
+
 ; 	; /// Activation Collision \\\
 ; 	bit 5, c
 ; 	jr z, @noReactCollision
-	
+
 ; 	;getting function address
 ; 	ld h, d
 ; 	ld l, e
@@ -88,7 +95,7 @@ collisionSolverIsaac:
 ; ;	call (hl)
 ; 	; \\\ Activation Collision ///
 ; @noReactCollision:
-	
+
 	pop de
 	pop bc
 	ret
