f501d9ad14a8cd12e989fc95d6a5d79069b34571
Thomas Grenier <thomas.grenier@telecom-paris.fr>
2020-06-26 15:56:30 +0200

    display the first rock

--- src/init/display_test.init.s
+++ src/init/display_test.init.s
@@ -12,14 +12,15 @@ inc l
 
 // Add rocks
 ld l,ROCKS_SPRITESHEET ;start tile id
-ld a, 12*8 ;X position in pixels (must be a multiple of 8)
-ld b, 12*8 ;Y position in pixels (must be a multiple of 8)
+ld a, (global_.elements.1.y) ;X position in pixels (must be a multiple of 8)
+ld b, a
+ld a, (global_.elements.1.x) ;Y position in pixels (must be a multiple of 8)
 call displayBackgroundTile
-ld l,ROCKS_SPRITESHEET+8 ;start tile id
-ld a, 7*8 ;X position in pixels (must be a multiple of 8)
-ld b, 4*8 ;Y position in pixels (must be a multiple of 8)
-call displayBackgroundTile
-ld l,ROCKS_SPRITESHEET+4 ;start tile id
-ld a, 3*8 ;X position in pixels (must be a multiple of 8)
-ld b, 8*8 ;Y position in pixels (must be a multiple of 8)
-call displayBackgroundTile
+; ld l,ROCKS_SPRITESHEET+8 ;start tile id
+; ld a, 7*8 ;X position in pixels (must be a multiple of 8)
+; ld b, 4*8 ;Y position in pixels (must be a multiple of 8)
+; call displayBackgroundTile
+; ld l,ROCKS_SPRITESHEET+4 ;start tile id
+; ld a, 3*8 ;X position in pixels (must be a multiple of 8)
+; ld b, 8*8 ;Y position in pixels (must be a multiple of 8)
+; call displayBackgroundTile
