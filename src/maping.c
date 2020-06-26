2d9c6d13a9868869fbe84b9a1373ee677c6c9507
melissa <melissa.blancheb@gmail.com>
2020-06-26 23:19:11 +0200

    advances on map generator

--- src/maping.c
+++ src/maping.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 unsigned char aleatNumber();
-unsigned char * unvisitedNeighbours1(unsigned char k);
-unsigned char * unvisitedNeighbours2(unsigned char k);
+unsigned char unvisitedNeighbours(unsigned char k);
 unsigned char * neighbours(unsigned char k);
 void display();
+void binaryDisplay(unsigned char n);
+unsigned char anyNeighbours(unsigned char k);
 
 unsigned char x=0;
 unsigned char y=0;
@@ -16,6 +17,7 @@ unsigned char hauteur;
 unsigned char largeur;
 unsigned char * map;
 unsigned char current;
+unsigned char temp[4];
 
 int main(int argc, char const *argv[]) {
   printf("level : %d\n",level);
@@ -47,58 +49,312 @@ int main(int argc, char const *argv[]) {
   }
   i--;
   j--;
-  map[j * largeur + i] = 0b00001001;
+  map[j * largeur + i] = 0b00001100;
   display();
 
   //Salle de boss
+  //Way with a length between m/3 and  n
+
   unsigned char diff = hauteur - largeur/3 ;
   unsigned char c = aleatNumber() % (diff + 1) ;
-  c = c + m/3;
-  //char * temp;
-  //char currentX = i;
-  /*whar currentY = j;
+  c = c + 1 + largeur/3;
+
+  current = j * largeur + i;
   while (c > 0){
-    //temp = unvisitedNeighbours(map, currentX, currentY, m, n);
+    unsigned char next = unvisitedNeighbours(current);
     c--;
-  }*/
-  //Chemin de longueur commprise entre min m/3 et max n
+    map[next] = map[next] | 0b00001000;
+    if (next == current + 1){
+      map[current] = map[current] | 0b00010000;
+      map[next] = map[next] | 0b00100000;
+    } else{
+      if (next == current - 1){
+        map[current] = map[current] | 0b00100000;
+        map[next] = map[next] | 0b00010000;
+      } else{
+        if (next == current - largeur){
+          map[current] = map[current] | 0b10000000;
+          map[next] = map[next] | 0b01000000;
+        } else{
+          if(next == current + largeur){
+            map[current] = map[current] | 0b01000000;
+            map[next] = map[next] | 0b10000000;
+          } else{
+            printf("Probleme dans le switch case !\n");
+          }
+        }
+      }
+    }
+    current = next;
+    printf("%d\n", next);
+  }
+  map[current] = map[current] & 0b11111000;
+  map[current] = map[current] | 0b00000010;
+  display();
 
-//item same/2
-
-//cul de sac
-//min n/2
-//max 3n/4
-
-  //Salle d'item
-
-  int k = 0;
-
-  /*for (int k = 0; k < 10; k++){
-
-    aleatNumber(a,x,y,z);
-  }*/
+  for (int i = 0; i < hauteur*largeur; i++){
+    map[i] = map[i] & 0b11110111;
+  }
 
 
-  //Up | Down | Left | Right | Visited | Visited2 | Cat | Cat
-  //00 : normal
-  //01 : depart
-  //10 : item
-  //11 : boss*/
+  //item same/2
+  a=a-2;
+  x=x+2;
+  y=y*2;
+  z=z-3;
+  diff = hauteur/2 - largeur/6 ;
+  c = aleatNumber() % (diff + 1) ;
+  c = c + largeur/6;
+
+  current = j * largeur + i;
+  while (c > 0){
+    unsigned char next = unvisitedNeighbours(current);
+    c--;
+    map[next] = map[next] | 0b00001000;
+    if (next == current + 1){
+      map[current] = map[current] | 0b00010000;
+      map[next] = map[next] | 0b00100000;
+    } else{
+      if (next == current - 1){
+        map[current] = map[current] | 0b00100000;
+        map[next] = map[next] | 0b00010000;
+      } else{
+        if (next == current - largeur){
+          map[current] = map[current] | 0b10000000;
+          map[next] = map[next] | 0b01000000;
+        } else{
+          if(next == current + largeur){
+            map[current] = map[current] | 0b01000000;
+            map[next] = map[next] | 0b10000000;
+          } else{
+            printf("Probleme dans le switch case !\n");
+          }
+        }
+      }
+    }
+    current = next;
+    printf("%d\n", next);
+  }
+  map[current] = map[current] & 0b11111000;
+  map[current] = map[current] | 0b00000001;
+  display();
+
+  for (int i = 0; i < hauteur*largeur; i++){
+    map[i] = map[i] & 0b11110111;
+  }
+
+  //cul de sac between n/2 and max 3n/4
+  //First
+  a=a*2;
+  x=x-2;
+  y=y*3;
+  z=z-2;
+  diff = 3*hauteur/4 - hauteur/2 ;
+  c = aleatNumber() % (diff + 1) ;
+  c = c + hauteur/2;
+
+  current = j * largeur + i;
+  while (c > 0){
+    unsigned char next = unvisitedNeighbours(current);
+    c--;
+    map[next] = map[next] | 0b00001000;
+    if (next == current + 1){
+      map[current] = map[current] | 0b00010000;
+      map[next] = map[next] | 0b00100000;
+    } else{
+      if (next == current - 1){
+        map[current] = map[current] | 0b00100000;
+        map[next] = map[next] | 0b00010000;
+      } else{
+        if (next == current - largeur){
+          map[current] = map[current] | 0b10000000;
+          map[next] = map[next] | 0b01000000;
+        } else{
+          if(next == current + largeur){
+            map[current] = map[current] | 0b01000000;
+            map[next] = map[next] | 0b10000000;
+          } else{
+            printf("Probleme dans le switch case !\n");
+          }
+        }
+      }
+    }
+    current = next;
+    printf("%d\n", next);
+  }
+  display();
+
+
+  //Second
+  a=a-1;
+  x=x+1;
+  y=y*3;
+  z=z-4;
+  diff = hauteur/2 - largeur/6 ;
+  c = aleatNumber() % (diff + 1) ;
+  c = c + largeur/6;
+
+  current = j * largeur + i;
+  while (c > 0){
+    unsigned char next = unvisitedNeighbours(current);
+    c--;
+    map[next] = map[next] | 0b00001000;
+    if (next == current + 1){
+      map[current] = map[current] | 0b00010000;
+      map[next] = map[next] | 0b00100000;
+    } else{
+      if (next == current - 1){
+        map[current] = map[current] | 0b00100000;
+        map[next] = map[next] | 0b00010000;
+      } else{
+        if (next == current - largeur){
+          map[current] = map[current] | 0b10000000;
+          map[next] = map[next] | 0b01000000;
+        } else{
+          if(next == current + largeur){
+            map[current] = map[current] | 0b01000000;
+            map[next] = map[next] | 0b10000000;
+          } else{
+            printf("Probleme dans le switch case !\n");
+          }
+        }
+      }
+    }
+    current = next;
+    printf("%d\n", next);
+  }
+  map[current] = map[current] & 0b11111000;
+  map[current] = map[current] | 0b00000001;
+  display();
+
+
+  //Up | Down | Left | Right | Visited | Cat | Cat | Cat
+  //000 : normal
+  //001 : item
+  //010 : boss
+  //100 : start
   return 0;
 }
 
 unsigned char * neighbours(unsigned char k){
-  char[4] temp;
   temp[0] = k - largeur;
-  tamp[1] = k - 1;
+  temp[1] = k - 1;
   temp[2] = k + 1;
   temp[3] = k + largeur;
   return temp;
 }
 
 
-unsigned char * unvisitedNeighbours1(unsigned char k){
+unsigned char unvisitedNeighbours(unsigned char k){
+  unsigned char * possibleNeighbours;
+  possibleNeighbours = neighbours(k);
+  int compteur = 0;
+  char neighbour;
+  char bool[4];
+  //for(int i = 0; i < 4; i++){
+  neighbour = possibleNeighbours[0];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k < largeur){
+      bool[0] = 0;
+    } else{
+      bool[0] = 1;
+      compteur++;
+    }
+  }
+  neighbour = possibleNeighbours[1];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k % largeur == 0){
+      bool[1] = 0;
+    } else{
+      bool[1] = 1;
+      compteur++;
+    }
+  }
+  neighbour = possibleNeighbours[2];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k % largeur == largeur - 1){
+      bool[2] = 0;
+    } else{
+      bool[2] = 1;
+      compteur++;
+    }
+  }
+  neighbour = possibleNeighbours[3];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k >= (hauteur - 1) * largeur){
+      bool[3] = 0;
+    } else{
+      bool[3] = 1;
+      compteur++;
+    }
+  }
+  char alea = aleatNumber() % compteur;
+  int i = 0;
+  char chose;
+  while(alea >= 0){
+    if (bool[i] == 1){
+      chose = possibleNeighbours[i];
+      alea--;
+    }
+    i++;
+  }
+  return chose;
+}
 
+unsigned char anyNeighbours(unsigned char k){
+  unsigned char * possibleNeighbours;
+  possibleNeighbours = neighbours(k);
+  int compteur = 0;
+  char neighbour;
+  char bool[4];
+  //for(int i = 0; i < 4; i++){
+  neighbour = possibleNeighbours[0];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k < largeur){
+      bool[0] = 0;
+    } else{
+      bool[0] = 1;
+      compteur++;
+    }
+  }
+  neighbour = possibleNeighbours[1];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k % largeur == 0){
+      bool[1] = 0;
+    } else{
+      bool[1] = 1;
+      compteur++;
+    }
+  }
+  neighbour = possibleNeighbours[2];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k % largeur == largeur - 1){
+      bool[2] = 0;
+    } else{
+      bool[2] = 1;
+      compteur++;
+    }
+  }
+  neighbour = possibleNeighbours[3];
+  if (!(map[neighbour] & (1u << 3))){
+    if (k >= (hauteur - 1) * largeur){
+      bool[3] = 0;
+    } else{
+      bool[3] = 1;
+      compteur++;
+    }
+  }
+  char alea = aleatNumber() % compteur;
+  int i = 0;
+  char chose;
+  while(alea >= 0){
+    if (bool[i] == 1){
+      chose = possibleNeighbours[i];
+      alea--;
+    }
+    i++;
+  }
+  return chose;
 }
 
 unsigned char aleatNumber(){
@@ -113,16 +369,25 @@ unsigned char aleatNumber(){
     z=a;
     a = z ^ t ^ ( z >> 1) ^ (t << 1);
   }
-  printf("%hhu\n", a%4);
+  //printf("%hhu\n", a%4);
   return a;
 }
 
 void display(){
   for (int k = 0 ; k < hauteur ; k++){
     for (int l = 0 ; l < largeur ; l++){
-      printf("%d   ", map[k * largeur + l]);
+      binaryDisplay(map[k * largeur + l]);
+      printf("    ");
     }
     printf("\n");
   }
   printf("\n");
+}
+
+void binaryDisplay(unsigned char n) {
+  int i = 7;
+  while (i >= 0){
+    printf("%d", (n >> i ) & 1);
+    i--;
+  }
 }
