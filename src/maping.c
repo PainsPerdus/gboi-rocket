2e87c52c907ec6e21569577db84c5855091dd13c
melissa <melissa.blancheb@gmail.com>
2020-06-27 22:29:15 +0200

    add comments

--- src/maping.c
+++ src/maping.c
@@ -3,7 +3,7 @@
 
 void createWay(unsigned char c);
 unsigned char aleatNumber();
-unsigned char unvisitedNeighbours(unsigned char k);
+unsigned char unvisitedNeighbour(unsigned char k);
 unsigned char * neighbours(unsigned char k);
 void display();
 void binaryDisplay(unsigned char n);
@@ -24,17 +24,26 @@ unsigned char boss;
 unsigned char item;
 
 int main(int argc, char const *argv[]) {
-  //printf("level : %d\n",level);
+  //Initialization of width and jeight on level
   level=level+6;
   height= level/2;
   width = level - height;
-  //printf("height : %d, width : %d\n", height, width);
+
+  //Initialization of the list map wich is a grid and will contain all the rooms
   map = (unsigned char *) malloc (height * width);
   for (int i = 0; i < height*width; i++){
-    map[i] = 0b00000000;
+    map[i] = 0b00000000;//Initialisation of each box = rooms
+    //Each room is represented by a byte
+    //Here each room is a normal room without any door and unvisited
+    //Room format (same than first byte int the room file Format in the wiki)
+    //Up | Down | Left | Right | Visited | Cat | Cat | Cat
+    //000 : normal
+    //001 : item
+    //010 : boss
+    //100 : start
   }
 
-  //Salle de depart
+  //Start room in the middle of the grid
   unsigned char i;
   unsigned char j;
   if (width%2 != 0){
@@ -54,78 +63,61 @@ int main(int argc, char const *argv[]) {
   i--;
   j--;
   start = j * width + i;
-  map[start] = 0b00001100;
-  //display();
+  map[start] = 0b00001100;//Put the start room as a start and visited room
 
   //Boss's room with a Way of a length between width/3 and height
 
   unsigned char diff = height - width/3 ;
-  unsigned char c = aleatNumber() % (diff + 1) ;
+  unsigned char c = aleatNumber() % (diff + 1) ;//generate a random length of the way between the limits mentionned above
   c = c + 1 + width/3;
 
   createWay(c);
 
   map[current] = map[current] & 0b11111000;
-  map[current] = map[current] | 0b00000010;
-  //display();
+  map[current] = map[current] | 0b00000010;//Put the final room as the boss's room
 
   for (int i = 0; i < height*width; i++){
-    map[i] = map[i] & 0b11110111;
+    if (i != start){
+      map[i] = map[i] & 0b11110111; //put back all room as unvisited rooms except the start room
+    }
   }
 
   //Item's room with way of a length between width/6 and height/2
-  a=a-2;
-  x=x+2;
-  y=y*2;
-  z=z-3;
   diff = height/2 - width/6 ;
-  c = aleatNumber() % (diff + 1) ;
+  c = aleatNumber() % (diff + 1) ;//generate a random length of the way between the limits mentionned above
   c = c + width/6;
 
   createWay(c);
 
   map[current] = map[current] & 0b11111000;
-  map[current] = map[current] | 0b00000001;
-  //display();
+  map[current] = map[current] | 0b00000001;//Put the final room as the boss's room
 
   for (int i = 0; i < height*width; i++){
-    map[i] = map[i] & 0b11110111;
+    if (i != start){
+      map[i] = map[i] & 0b11110111;//Put back all the rooms unvisited rooms except the start room
+    }
   }
 
   //Dead ends between height/2 and max 3*height/4
-  //First
-  a=a*2;
-  x=x-2;
-  y=y*3;
-  z=z-2;
+  //First dead end
   diff = 3*height/4 - height/2 ;
-  c = aleatNumber() % (diff + 1) ;
+  c = aleatNumber() % (diff + 1) ;//generate a random length of the way between the limits mentionned above
   c = c + height/2;
 
   createWay(c);
-  //display();
 
-
-  //Second
-  a=a-1;
-  x=x+1;
-  y=y*3;
-  z=z-2;
+  //Second dead end
   diff = height/2 - width/6 ;
-  c = aleatNumber() % (diff + 1) ;
+  c = aleatNumber() % (diff + 1) ;//generate a random length of the way between the limits mentionned above
   c = c + width/6;
 
   createWay(c);
   //display();
 
-  //Up | Down | Left | Right | Visited | Cat | Cat | Cat
-  //000 : normal
-  //001 : item
-  //010 : boss
-  //100 : start
   return 0;
 }
 
+//Give the subscripts of the 4 theoric rooms next to the room correcponding to the subscript in argument
 unsigned char * neighbours(unsigned char k){
   temp[0] = k - width;
   temp[1] = k - 1;
@@ -134,54 +126,57 @@ unsigned char * neighbours(unsigned char k){
   return temp;
 }
 
-
-unsigned char unvisitedNeighbours(unsigned char k){
+//one random unvisited and accessible room next to the room correcponding to the subscript in argument
+unsigned char unvisitedNeighbour(unsigned char k){
   unsigned char * possibleNeighbours;
-  possibleNeighbours = neighbours(k);
-  int compteur = 0;
+  possibleNeighbours = neighbours(k);//Give theoric neighbours
+  int compteur = 0;//will count accessible unvisited rooms
   unsigned char neighbour;
-  char bool[4];
-  //for(int i = 0; i < 4; i++){
+  char bool[4];//will contain booleans about if the room is (accessible and unvisited)
+  //For the room above
   neighbour = possibleNeighbours[0];
-  if (!(map[neighbour] & (1u << 3))){
-    if (k < width){
+  if (!(map[neighbour] & (1u << 3))){//check if it is unvisited
+    if (k < width){//check if it is accessible
       bool[0] = 0;
     } else{
       bool[0] = 1;
-      compteur++;
+      compteur++;//count unvisited accessible rooms
     }
   }
+  //For the room on the left
   neighbour = possibleNeighbours[1];
-  if (!(map[neighbour] & (1u << 3))){
-    if (k % width == 0){
+  if (!(map[neighbour] & (1u << 3))){//check if it is unvisited
+    if (k % width == 0){//check if it is accessible
       bool[1] = 0;
     } else{
       bool[1] = 1;
-      compteur++;
+      compteur++;//count unvisited accessible rooms
     }
   }
+  //for the room on the right
   neighbour = possibleNeighbours[2];
-  if (!(map[neighbour] & (1u << 3))){
-    if (k % width == width - 1){
+  if (!(map[neighbour] & (1u << 3))){//check if it is unvisited
+    if (k % width == width - 1){//check if it is accessible
       bool[2] = 0;
     } else{
       bool[2] = 1;
-      compteur++;
+      compteur++;//count unvisited accessible rooms
     }
   }
+  //for the room below
   neighbour = possibleNeighbours[3];
-  if (!(map[neighbour] & (1u << 3))){
-    if (k >= (height - 1) * width){
+  if (!(map[neighbour] & (1u << 3))){//check if it is unvisited
+    if (k >= (height - 1) * width){//check if it is accessible
       bool[3] = 0;
     } else{
       bool[3] = 1;
-      compteur++;
+      compteur++;//count unvisited accessible rooms
     }
   }
-  char alea = aleatNumber() % compteur;
+  char alea = aleatNumber() % compteur;//chose a random room in thos unvisited and accessible
   int i = 0;
   char chose;
-  while(alea >= 0){
+  while(alea >= 0){//Circle to find the alea-nd unvisited accessible rooms
     if (bool[i] == 1){
       chose = possibleNeighbours[i];
       alea--;
@@ -191,11 +186,7 @@ unsigned char unvisitedNeighbours(unsigned char k){
   return chose;
 }
 
-unsigned char aleatNumber(){
-  y=y*2;
-  x=x-1;
-  z=z+6;
-  a++;
+unsigned char aleatNumber(){//Have a random number
   for(unsigned long i = 0 ; i < 14 ; i++) {
     unsigned char t = x ^ (x << 4);
     x=y;
@@ -203,16 +194,16 @@ unsigned char aleatNumber(){
     z=a;
     a = z ^ t ^ ( z >> 1) ^ (t << 1);
   }
-  //printf("%hhu\n", a%4);
   return a;
 }
 
-void createWay(unsigned char c){
-  current = start;
-  while (c > 0){
-    unsigned char next = unvisitedNeighbours(current);
+void createWay(unsigned char c){//create a random way from the start to a specific room with the length mentionned in argument
+  current = start;//the way start in the start room
+  while (c > 0){//until the length of the way is not reached
+    unsigned char next = unvisitedNeighbour(current);//take a random unvisited accessible neighbour
     c--;
-    map[next] = map[next] | 0b00001000;
+    map[next] = map[next] | 0b00001000;//set the new room as visited
+    //The following code aim to open the right door between the previous and the next room in different cases
     if (next == current + 1){
       map[current] = map[current] | 0b00010000;
       map[next] = map[next] | 0b00100000;
@@ -228,18 +219,16 @@ void createWay(unsigned char c){
           if(next == current + width){
             map[current] = map[current] | 0b01000000;
             map[next] = map[next] | 0b10000000;
-          } else{
-            printf("Probleme dans le switch case !\n");
           }
         }
       }
     }
-    current = next;
-    //printf("%d\n", next);
+    current = next;//Move on next room
   }
 }
 
-void display(){
+//// Specific C code to display and check the code above \\\\
+void display(){//to show the map of rooms, each room is represented by a byte
   for (int k = 0 ; k < height ; k++){
     for (int l = 0 ; l < width ; l++){
       binaryDisplay(map[k * width + l]);
@@ -250,10 +239,12 @@ void display(){
   printf("\n");
 }
 
-void binaryDisplay(unsigned char n) {
+void binaryDisplay(unsigned char n) {//to show a char in its binary form
   int i = 7;
   while (i >= 0){
     printf("%d", (n >> i ) & 1);
     i--;
   }
 }
+
+//// Specific C code to display and check the code above \\\\
