588435382c022b751091d358f107acd9583969bf
Thomyrock <66800538+Thomyrock@users.noreply.github.com>
2020-06-29 21:25:28 +0200

    invert x y in display_test.init

--- src/init/display_test.init.s
+++ src/init/display_test.init.s
@@ -12,8 +12,8 @@ display_test_init:
 	ld h,d
 	ld l,e
 	inc hl
-	ldi a, (hl) ;x position in pixels (must be a multiple of 8)
-	ld b, (hl) ;y position in pixels (must be a multiple of 8)
+	ldi b, (hl) ;y position in pixels (must be a multiple of 8)
+	ld a, (hl) ;x position in pixels (must be a multiple of 8)
 	ld l,ROCKS_SPRITESHEET ;start tile id
 	call displayBackgroundTile
 @ending_of_looptodisplayrocks:
@@ -35,8 +35,8 @@ display_test_init:
 	ld h,d
 	ld l,e
 	inc hl
-	ldi a, (hl) ;x position in pixels (must be a multiple of 8)
-	ld b, (hl) ;y position in pixels (must be a multiple of 8)
+	ldi b, (hl) ;y position in pixels (must be a multiple of 8)
+	ld a, (hl) ;x position in pixels (must be a multiple of 8)
 	ld l,ROCKS_SPRITESHEET ;start tile id
 	call displayBackgroundTile
 @ending_of_looptodisplayhurtingrocks:
