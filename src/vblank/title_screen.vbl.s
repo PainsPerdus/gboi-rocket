670ad9e6fc87793234eb3be2c7244a0cb5245b53
Virgile Retault <virgile.retault@telecom-paris.fr>
2020-07-01 01:16:49 +0200

    added blank titlescreen, press start (enter in emulator) to start game

--- src/vblank/title_screen.vbl.s
+++ src/vblank/title_screen.vbl.s
@@ -1,4 +1,11 @@
 titleScreen:
-	ld b,b
+	
+	ld a,%00010000 ; Selection touches de direction
+	ldh ($00),a ;($FF00)=$20
+
+	ldh a,($00) ;B=($FF00) lire état touches
+	bit 3,a
+	jr nz, @noSelect
 	ld a, GAMESTATE_PLAYING
 	call setGameState
+@noSelect:
