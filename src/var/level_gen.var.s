706c311bb5f8bc2b0336395eba974cb6a8d3704b
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-06-30 10:38:08 +0200

    Added struct for level generation

---
+++ src/var/level_gen.var.s
@@ -0,0 +1,16 @@
+.STRUCT level_gen_var
+	level DB
+    height DB
+    width DB
+    map DSB 256
+    list DSB 128
+    current DB
+    temp DSB 4
+    start DB
+    boss DB
+    item DB
+    basicNumber DB
+    itemNumber DB
+    bossNumber DB
+    openedRooms DB
+.ENDST
