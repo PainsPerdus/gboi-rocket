519b08939b63e1c9202b20abda59ae637e307445
Virgile Retault <virgile.retault@telecom-paris.fr>
2020-07-01 01:29:36 +0200

    added intro screen sprites in lib

--- src/init/title_screen.init.s
+++ src/init/title_screen.init.s
@@ -1,3 +1,67 @@
+title_screen:
+
+; ///// Copy Tiles \\\\\
+	
+; ///// BANK 1 \\\\\
+
+; \\\\\ BANK 1 /////
+
+
+; ///// BANK 2 \\\\\
+
+; \\\\\ BANK 2 /////
+
+; ///// BANK 3 \\\\\
+
+; \\\\\ BANK 3 /////
+
+    ld bc,(BACKGROUND_TILES_NUMBER+1)*16
+    ld de,BackgroundTiles
+    ld hl,BACKGROUND_TILES_START_ADDRESS
+@loopBackgroundTiles: ; while bc != 0
+    ld a,(de)
+    ldi (hl),a  ; *hl <- *de; hl++
+    inc de          ; de ++
+    dec bc ; bc--
+    ld a,b
+    or c
+    jr nz,@loopBackgroundTiles  ; end while
+; \\\\\ Copy Background Tiles /////
+
+; /////// CLEAR BG \\\\\\\
+    ld de,32*32
+    ld hl,$9800
+@clmap:         ; while de != 0
+    xor a;
+    ldi (hl),a  ; *hl <- 0; hl++
+    dec de      ; de --
+    ld a,e
+    or d
+    jr nz,@clmap    ; end while
+    ; The Z flag can't be used when dec on 16bit reg :(
+; \\\\\\\ CLEAR BG ///////
+
+; /////// CLEAR OAM \\\\\\\
+    ld hl,$FE00
+    ld b,40*4
+@clspr:         ; while b != 0
+    ld (hl),$00 ; *hl <- O
+    inc l       ; hl ++ (no ldi hl or inc hl: bug hardware!)
+    dec b       ; b --
+    jr nz,@clspr    ; end while
+            ; set the background offset to 0
+    xor a
+    ldh ($42),a
+    ldh ($43),a
+; \\\\\\\ CLEAR OAM ///////
+
+; /////// INIT COLOR PALETTES \\\\\\\
+ld a,%11100100  ; 11=Black 10=Dark Grey 01=Grey 00=White/trspt
+ldh ($47),a ; background palette
+ldh ($48),a ; sprite 0 palette
+ldh ($49),a ; sprite 1 palette
+; \\\\\\\ INIT COLOR PALETTES ///////
+
 ; /////// ENABLE SCREEN \\\\\\\
 ld a,%10000011  ; screen on, bg on, tiles at $8000
 ldh ($40),a
