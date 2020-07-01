76c88812eac3d6312c98eaf4c16159e1237970a9
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-07-01 11:23:36 +0200

    enabled timer

--- src/init/music.init.s
+++ src/init/music.init.s
@@ -4,5 +4,9 @@ music_init:
     ld a,%01110111
     ldh ($24),a    ;set channels' volume to the maximum
 
+    ldh a, ($FF) ; enable timer
+    or %00000100
+    ldh ($FF), a
+
     ld hl, sacrificial_music
     call music_start
