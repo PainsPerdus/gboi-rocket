768dcb43c283cb9e9eb9670949e05258458770e8
Thomas Grenier <grenier.thomas@laposte.net>
2024-09-28 09:08:03 +0200

    properly comment tears_dmg.s

--- src/tears_dmg.s
+++ src/tears_dmg.s
@@ -4,87 +4,88 @@ isaac_tears_dmg:
 
 	ld c,n_isaac_tears
 @loop_tears:
-	ld h,d
+	ld h,d  ; tear ptr to hl
 	ld l,e
-	ld a,(hl)
-	and a
-	jp z,@ending_loop_tears
+	ld a,(hl)  ; tear to a
+	and a  ; tear is not none ?
+	jp z,@ending_loop_tears  ; if none continue
 
-	ldi a, (hl)
+	ldi a, (hl)  ; load y tear
 	add TEARS_OFFSET_Y
 	ld (collision_.p.1.y), a
 	ld (collision_.p_RD.1.y), a
-	ld a,(hl)
+	ld a,(hl)  ; load x tear
 	add TEARS_OFFSET_X
 	ld (collision_.p.1.x), a
 	ld (collision_.p_RD.1.x), a
 
-	push de
+	push de  ; push tear ptr
 	ld de,global_.enemies
 	ld b,n_enemies
 @loop_ennemies:
-	ld h,d
+	ld h,d  ; ennemy ptr to hl
 	ld l,e
-	ldi a,(hl)
-	bit ALIVE_FLAG, a
-	jr z,@ending_loop_ennemies
-	and ENEMY_SIZE_MASK
+	ldi a,(hl) ; ennemy info to a
+	bit ALIVE_FLAG, a  ; is alive ?
+	jr z,@ending_loop_ennemies  ; if not continue
+	and ENEMY_SIZE_MASK  ; ennemy size to a
 	ld (collision_.hitbox2),a
-	ldi a,(hl)
+	ldi a,(hl)  ; load ennemy y
 	ld (collision_.p.2.y),a
-	ldi a,(hl)
+	ldi a,(hl)  ; load ennemy x
 	ld (collision_.p.2.x),a
 	call preloaded_collision
-	and a
-	jp z,@ending_loop_ennemies
+	and a  ; collison ?
+	jp z,@ending_loop_ennemies  ; if not continue
 
 ; //// DEAL DMG \\\\
 	; kill tear
-	ld h, d
+    ; TODO: this is needlessly complicated ?
+	ld h, d  ; ennemy ptr to hl
 	ld l, e
-	pop de ; the hitting tear
-	push hl
-	ld h,d
+	pop de  ; pop tear ptr TODO: make sure of that
+	push hl  ; push ennemy ptr
+	ld h,d  ; tear ptr to hl
 	ld l,e
 	xor a
-	ld (hl),a ; kill the tear
+	ld (hl),a  ; tear becomes none
 
 	; hurt
 	call tear_hit_sfx
-	pop hl
-	ld d, h
+	pop hl  ; pop ennemy ptr
+	ld d, h  ; ennemy ptr to de
 	ld e, l
-	ld hl,3
-	add hl,de
-	ld a, (hl)
-	ld hl, global_.isaac.dmg
-	sub (hl)
-	ld hl,3
-	add hl, de
-	ld (hl), a
-	cp 1
+	ld hl,3  ; TODO: remove hardcoded value
+	add hl,de  ; ennemy hp ptr to hl
+	ld a, (hl)  ; ennemy hp to a
+	ld hl, global_.isaac.dmg  ; load dmg
+	sub (hl)  ; inflict dmg
+	ld hl,3  ; TODO: remove hardcoded value
+	add hl, de  ; ennemy hp ptr to hl
+	ld (hl), a  ; new hp to hp ptr
+	cp 1  ; TODO: check if that works
 	jp nc, @@notDead
-	ld a, (de)
-	res 7, a
-	ld (de), a
+	ld a, (de)  ; ennemy info to a
+	res 7, a  ; reset isAlive bit TODO: remove hardcoded
+	ld (de), a  ; write new ennemy info
 
 	; unlock room
-	ld a, (load_map_.mobs)
+	ld a, (load_map_.mobs)  ; reduce mob number
 	dec a
 	ld (load_map_.mobs), a
-	and a
+	and a  ; check of mob number is 0
 	jr nz, @@notDead
-	ld a, (current_floor_.current_room)
+	ld a, (current_floor_.current_room)  ; room ptr to hl
 	ld h, a
 	ld a, (current_floor_.current_room + 1)
 	ld l, a
+	inc hl  ; room info ptr to hl
 	inc hl
-	inc hl
-	ld a, (hl)
-	res 3, a
-	ld (hl), a
-	ld (load_map_.doors), a
-	ld a, GAMESTATE_CHANGINGROOM
+	ld a, (hl)  ; room info to a
+	res 3, a  ; open room ? TODO: remove hardcoded
+	ld (hl), a  ; write new room info
+	ld (load_map_.doors), a  ; also update in loaded representation
+	ld a, GAMESTATE_CHANGINGROOM  ; update room
 	jp setGameState
 ; \\\\ DEAL DMG ////
 @@notDead:
@@ -92,18 +93,18 @@ isaac_tears_dmg:
 
 @ending_loop_ennemies:
 	ld hl,_sizeof_enemy
-	add hl,de
-	ld d,h
+	add hl,de  ; next ennemy ptr to hl
+	ld d,h  ; new ennemy ptr to de
 	ld e,l
 
-	dec b
+	dec b  ; check if some ennemies are left
 	jr nz,@loop_ennemies
-	pop de
+	pop de  ; leaving loop ennemies, restore tear ptr
 @ending_loop_tears:
 	ld hl,_sizeof_tear
-	add hl,de
-	ld d,h
+	add hl,de  ; next tear ptr to hl
+	ld d,h  ; new tear ptr to de
 	ld e,l
 
-	dec c
+	dec c  ; check if some tears are left
 	jp nz,@loop_tears
