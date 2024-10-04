f6609f46d9b3736fa9265aa201665c631aa99a50
Thomas Grenier <grenier.thomas@laposte.net>
2024-10-04 19:36:21 +0200

    make second floor trivially generated

--- src/init/load_floor.init.s
+++ src/init/load_floor.init.s
@@ -1,32 +1,25 @@
 @load_floor:
 
 	ld a, (current_floor_.i_floor)
-
-    ; load floor
-    cp 0
-    jr nz, @@not_first
+	cp 0  ; first floor is hardcoded
+	jr nz, @not_first_floor
+	
+	; copy memery stored 1st floor into current_floor_
 	ld de, first_floor
-    jr @@floorReady
-@@not_first:
-    cp 1
-    jr nz, @@not_second
-    ld de, second_floor
-    jr @@floorReady
-@@not_second:
-
-@@floorReady
 	ld hl, current_floor_
-    ld a, _sizeof_current_floor_var - 3
+	ld a, _sizeof_current_floor_var - 3
 	ld b, a
-@initFloorLoop:
+@loadFirstFloorLoop:
 	ld a, (de)
 	ld (hl), a
 	inc de
 	inc hl
 	dec b
-	jr nz, @initFloorLoop
+	jr nz, @loadFirstFloorLoop
 
-; find first room
+	; find first room
+	; TODO: we could have as convention that the
+	;       starting room is the first room
 	ld a, (current_floor_.number_rooms)
 	ld b, a
 	ld de, current_floor_.rooms
@@ -49,7 +42,41 @@
 	ld (current_floor_.current_room), a
 	ld a, e
 	ld (current_floor_.current_room + 1), a
+	jr @floorReady
 
+@not_first_floor:
+	; randomly generate the floor
+	xor c  ; c is the current number of room
+	       ; don't take b as b is modified by rng
+
+	; generate the first room
+	ld de, current_floor_.rooms
+	ld a, d  ; set it as the current room
+	ld (current_floor_.current_room), a
+	ld a, e
+	ld (current_floor_.current_room + 1), a
+	call rng
+	ld h, d
+	ld l, e
+	ldi (hl), a  ; random coordinates
+	ld a, 0  ; first map always has id 0
+	ldi (hl), a
+	ld a, ROOM_TYPE_START
+	ldi (hl), a
+	ld a, $ff  ; masks with all bit set
+	ldi (hl), a
+	ldi (hl), a
+	ldi (hl), a
+	ldi (hl), a
+	ldi (hl), a
+	ldi (hl), a
+	ldi (hl), a
+	ld d, h  ; de is the currently built room
+	ld e, l
+	inc c
+
+
+@floorReady:
 ; load first room
 	ld a, (current_floor_.current_room)
 	ld h, a
